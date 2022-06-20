4e0d7ef4c92f0e3575a5c9508b49f2507e0338fe
israe <israelalves45155b@gmail.com>
2022-06-20 12:01:49 -0300

    adicionando json local

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ function createTask(textInput) {
   li.innerText = textInput;
   taskContainer.appendChild(li);
   cleanInput();
-  eraseButtonElement(li);
+  createButtonErase(li);
   saveTask();
 }
 
@@ -16,7 +16,7 @@ function createLi() {
   return li;
 }
 
-function eraseButtonElement(li) {
+function createButtonErase(li) {
   li.innerText += " ";
   const erase = document.createElement("button");
   erase.innerText = "apagar";
@@ -32,17 +32,26 @@ function cleanInput() {
 function saveTask() {
   const liTasks = document.querySelectorAll("li");
   const taskList = new Array();
-
   for (let task of liTasks) {
     let taskText = task.innerText;
-    taskText = taskText.replace("apagar", " ");
+    taskText = taskText.replace("apagar", " ").trim();
     taskList.push(taskText);
   }
-
   const taskJson = JSON.stringify(taskList);
   localStorage.setItem("tasks", taskJson);
 }
 
+function addSavedTasks() {
+  const tasks = localStorage.getItem("tasks");
+  const tasksList = JSON.parse(tasks);
+
+  console.log(tasksList);
+
+  for (let task of tasksList) {
+    createTask(task);
+  }
+}
+addSavedTasks();
 input.addEventListener("keypress", function (e) {
   if (e.keyCode == 13) createTask(input.value), cleanInput();
 });
@@ -54,7 +63,6 @@ button.addEventListener("click", function (e) {
 
 document.addEventListener("click", function (e) {
   const el = e.target;
-
   if (el.classList.contains("apagar")) {
     el.parentElement.remove();
     saveTask();
